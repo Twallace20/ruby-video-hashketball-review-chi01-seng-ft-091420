95cc20e53a3418cb319ce475a159964e20211df9
Tyler Wallace <tywallace2017@gmail.com>
2020-09-15 14:11:27 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -130,8 +130,9 @@ end
  def num_points_scored (player_name)
 
 game_hash.each do |team, team_info|
-  team_info[:players].each do |player|
-    if player 
+  team_info[:players].each do |players|
+    if player [:player_name]== player_search
+      return player[:points]
 end
 
    
